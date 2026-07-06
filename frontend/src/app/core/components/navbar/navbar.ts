06070f16956622f62f714ba63f76fafdd936e681
chju-orif <julien.chollet@formation.orif.ch>
2026-07-06 09:22:00 +0200

    style: remove assets sub-navigation row from navbar as requested

--- frontend/src/app/core/components/navbar/navbar.ts
+++ frontend/src/app/core/components/navbar/navbar.ts
@@ -1,18 +1,15 @@
 import { Component } from '@angular/core';
-import { LucideAngularModule, Home, Zap, TrendingUp, Landmark } from 'lucide-angular';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { LucideAngularModule, Home } from 'lucide-angular';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [LucideAngularModule, RouterLink, RouterLinkActive],
+  imports: [LucideAngularModule, RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
 
   readonly Home = Home;
-  readonly Zap = Zap;
-  readonly TrendingUp = TrendingUp;
-  readonly Landmark = Landmark;
 
 }
